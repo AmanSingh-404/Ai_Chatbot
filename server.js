640c35c34031d3c4af235fa941fa9bddcf3e0fe8
Aman Singh <amanscode2005@gmail.com>
2026-02-14 15:54:01 +0530

    integerate google gemini AI

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
+require("dotenv").config();
 const app = require("./src/app")
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const {genrateResponse} = require("./src/services/ai.services")
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, { /* options */ });
 
 io.on("connection", (socket) => {
-  // ...
+  console.log("A User Connected")
+
+  socket.on("disconnect",(socket)=>{
+    console.log("user Disconnected");
+  })
+
+  socket.on("ai-message", async (data)=>{
+    console.log("user-message", data);
+    const response = await genrateResponse(data.prompt);
+    console.log("ai-response", response)
+    socket.emit("ai-message-response", {response})
+  })
 });
 
 
