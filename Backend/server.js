7a06520f8043ae2a1f4083319a3187660be0330d
Aman Singh <amanscode2005@gmail.com>
2026-02-16 15:02:19 +0530

    ready to deploy

--- Backend/server.js
+++ Backend/server.js
@@ -2,52 +2,54 @@ require("dotenv").config();
 const app = require("./src/app")
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-const {genrateResponse} = require("./src/services/ai.services");
+const { genrateResponse } = require("./src/services/ai.services");
 const { text } = require("stream/consumers");
 
 const httpServer = createServer(app);
-const io = new Server(httpServer, { 
-  cors:{
-    origin:"http://localhost:5173"
+const io = new Server(httpServer, {
+  cors: {
+    origin: process.env.FRONTEND_URL || "http://localhost:5173"
   }
- });
+});
 
 const chatHistory = [
-  {role: 'user',
-  parts: [{ text: 'Turn the lights down to a romantic level' }]
-},
-{
-  role:"model",
-  parts:[
-    {
-      text:"welcome to the smart home"
-    }
-  ]
-}
+  {
+    role: 'user',
+    parts: [{ text: 'Turn the lights down to a romantic level' }]
+  },
+  {
+    role: "model",
+    parts: [
+      {
+        text: "welcome to the smart home"
+      }
+    ]
+  }
 ]
 
 io.on("connection", (socket) => {
   console.log("A User Connected")
 
-  socket.on("disconnect",(socket)=>{
+  socket.on("disconnect", (socket) => {
     console.log("user Disconnected");
   })
 
-  socket.on("ai-message", async (data)=>{
+  socket.on("ai-message", async (data) => {
     console.log("user-message", data);
 
     chatHistory.push({
-      role:"user",
-      parts:[{ text: data.prompt }]
+      role: "user",
+      parts: [{ text: data.prompt }]
     })
 
     const response = await genrateResponse(chatHistory);
     console.log("ai-response", response)
-    socket.emit("ai-message-response", {response})
+    socket.emit("ai-message-response", { response })
   })
 });
 
 
-httpServer.listen(3000, ()=>{
-    console.log("server running on port 3000");
+const PORT = process.env.PORT || 3000;
+httpServer.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
 })
